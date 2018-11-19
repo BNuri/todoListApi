422762ddd4a5ae833f81f9efb07d2d86466d801a
bnr <luckmon05@naver.com>
2018-11-19 23:44:33 +0900

    change status type

--- api/models/todoListModel.js
+++ api/models/todoListModel.js
@@ -12,11 +12,8 @@ var TaskSchema = new Schema({
     default: Date.now
   },
   status: {
-    type: [{
-      type: String,
-      enum: ['pending', 'ongoing', 'completed']
-    }],
-    default: ['pending']
+    type: Boolean,
+    default: false
   }
 });
 
